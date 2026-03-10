91c65b722ceb0fc0423f4b97ec0112647e93086e
19MH1A0511 <chittidiganesh999@gmail.com>
2026-03-10 12:31:36 +0530

    changed in email send

--- src/utils/sendemail.service.js
+++ src/utils/sendemail.service.js
@@ -13,6 +13,9 @@ export default class SendEmailService {
                     user: process.env.SMTP_USER,
                     pass: process.env.SMTP_PASSWORD,
                 },
+                tls: {
+                    rejectUnauthorized: false // <--- ignore self-signed cert
+                }
             });
 
             const info = await transporter.sendMail({
