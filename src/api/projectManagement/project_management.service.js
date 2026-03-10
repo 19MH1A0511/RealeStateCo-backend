67b1f6245f8b6d14ab737bd91e42c674d27281ca
19MH1A0511 <chittidiganesh999@gmail.com>
2026-03-10 10:34:45 +0530

    modify code in project

--- src/api/projectManagement/project_management.service.js
+++ src/api/projectManagement/project_management.service.js
@@ -5,12 +5,25 @@ import { ApiError } from "../../utils/apiError.js";
 const projectManagementRepository = new ProjectManagementRepository();
 
 export default class ProjectManagementService {
-    
+
     addProjectManagement = async (data) => {
+        if(data?.type){
+            if(data.type === "Interior"){
+                data.type = "interior_design";
+            };
+        };
         const mapData = {
-            projectName: data?.projectName?.trim() || null,
-            projectType: data?.projectType || null,
-            description: data?.description || null,
+            fristName: data?.firstName?.trim() || null,
+            lastName: data?.lastName?.trim() || null,
+            email: "",
+            contactNumber: data?.mobile || null,
+            alternativeContactNumber: data?.alternateMobile || null,
+            projectType: data?.type || null,
+            description: "",
+            address: data?.address || null,
+            city: data?.city || null,
+            pincode: data?.pinCode || null,
+            createdAt: new Date(),
             userId: Number(data?.userId),
         };
         return await projectManagementRepository.createProjectManagementInDb(mapData);
